045eba9bc116bb65118cc6fb031ea2d76df9f020
Laurence <laurenceweening@gmail.com>
2016-04-20 00:08:04 +0200

    Create seeds for Artist

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Artist.delete_all
+
+Artist.create(
+  name: "Lukas Graham",
+  country: "Denmark",
+  bio: "Lukas Graham is a Danish ghetto-pop band consisting of
+        Lukas Graham Forchhammer (born September 18, 1988)
+        (vocals), Mark 'Lovestick' Falgren (born November 28, 1988)
+        (drums), Magnus Larsson (bass) and Kasper Daugaard
+        (born June 14, 1989) (keyboard). Launching their debut
+        album in 2012 on Copenhagen Records, Lukas Graham quickly
+        became a popular live act in Denmark. That success
+        translated to the rest of Europe and caught the attention
+        of Warner Bros. Records who co-signed them in 2013.
+        The band’s music was first introduced worldwide with the
+        release of their two singles 'Mama Said' and '7 Years', the
+        latter of which topped various international charts.
+        Their second album was released internationally on April 1",
+  image: "graham.jpg"
+)
