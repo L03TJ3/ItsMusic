2fa60d7ad5c2ac17d2a412c43e64033701169ffb
Laurence <laurenceweening@gmail.com>
2016-04-20 14:29:00 +0200

    Extra seed data/ fix some layout errors

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,12 +19,41 @@ Artist.create(
   image: "graham.jpg"
 )
 
+Artist.create(
+  name: "Tupac Shakur",
+  country: "United States",
+  bio: "Tupac Amaru Shakur (/ˈtuːpɑːk ʃəˈkʊər/ too-pahk shə-koor;
+        born Lesane Parish Crooks; June 16, 1971 – September 13,
+        1996), also known by his stage names 2Pac and Makaveli,
+        was an American rapper and actor. As of 2007, Shakur
+        has sold over 75 million records worldwide. His double
+        disc albums All Eyez on Me and his Greatest Hits are
+        among the best selling albums in the United States. He
+        has been listed and ranked as one of the greatest artists
+        of all time by many magazines, including Rolling Stone
+        which ranked him 86th on its list of The 100 Greatest
+        Artists of All Time.[5] He is consistently ranked as one
+        of the greatest rappers ever, as well as one of the most
+        influential rappers of all time.[6]Lukas Graham is a
+        Danish ghetto-pop band consisting of"
+  image: "tupac.jpg"
+)
 Song.delete_all
 
 Song.create(
   title: "7 years",
   artist_id: 1,
   year: 2015,
+  chorus: "Once I was seven years old, my momma told me
+           Go make yourself some friends or you'll be lonely
+           Once I was seven years old",
+  cover: "7years.jpg"
+)
+
+Song.create(
+  title: "Ghetto Gospel (with elton john)",
+  artist_id: 2,
+  year: 2004,
   chorus: "Once I was seven years old, my momma told me
            Go make yourself some friends or you'll be lonely
            Once I was seven years old",
