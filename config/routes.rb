caf011974f712a75c103f540849339677da2e9ec
Laurence <laurenceweening@gmail.com>
2016-04-27 17:30:09 +0200

    devise alternate login form / some styling tweaks

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
+
   resources :artists do
     resources :songs
   end
