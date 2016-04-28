edf131bc81bbe8fc1cf6e704ebb3c823d14d17e8
Laurence <laurenceweening@gmail.com>
2016-04-28 22:48:56 +0200

    Add artist (still bugs)

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -11,4 +11,29 @@ class ArtistsController < ApplicationController
       flash[:alert] = message
       redirect_to root_path
   end
+
+  def new
+    @artist = Artist.new
+  end
+
+  def create
+    @artist = Artist.new(artist_params)
+
+    respond_to do |format|
+      if @artist.save
+        format.html { redirect_to @artist, notice: 'Artist was successfully added'}
+        format.json { render :show, status: :created, location: @course}
+      else
+        format.html { render :new}
+        format.json { render json: @artist.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+
+  protected
+
+    def artist_params
+      params.require(:artist).permit(:name, :country, :bio, :ar_cover)
+    end
 end
--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,4 +1,5 @@
 class Artist < ActiveRecord::Base
   has_many :songs
-  mount_uploader :image, ImageUploader
+  mount_uploader :ar_cover, ImageUploader
+  validates_presence_of :name, :country, :bio, :ar_cover
 end
